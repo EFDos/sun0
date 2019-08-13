6544d1553f0e563c712a5f4b692664a0d4f24116
Douglas Muratore <douglas.muratore@gmail.com>
2019-08-13 11:56:35 -0300

    improving external modular includes

--- src/engine/gen_headers.sh
+++ src/engine/gen_headers.sh
@@ -2,3 +2,4 @@
 
 find . -type d -name '*' -exec mkdir -p ../../include/sun0/{} \;
 find . -type f -name '*.hpp' -exec cp {} ../../include/sun0/{} \;
+find . -type f -name '*.hpp.in' -exec cp {} ../../include/sun0/{} \;

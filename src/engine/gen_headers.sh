edd67de82eaa8a4c5e80967cc15a789ea41c5309
Douglas Muratore <douglas.muratore@gmail.com>
2019-08-16 02:26:36 -0300

    added cpptoml for Toml support

--- src/engine/gen_headers.sh
+++ src/engine/gen_headers.sh
@@ -2,4 +2,5 @@
 
 find . -type d -name '*' -exec mkdir -p ../../include/sun0/{} \;
 find . -type f -name '*.hpp' -exec cp {} ../../include/sun0/{} \;
+find . -type f -name '*.h' -exec cp {} ../../include/sun0/{} \;
 find . -type f -name '*.hpp.in' -exec cp {} ../../include/sun0/{} \;

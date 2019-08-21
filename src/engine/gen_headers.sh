1319a9c16d4aaea518e12ea8b09201408cd78480
Douglas Muratore <douglas.muratore@gmail.com>
2019-08-21 20:26:02 -0300

    got rid of broken external includes generation

--- src/engine/gen_headers.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-find . -type d -name '*' -exec mkdir -p ../../include/sun0/{} \;
-find . -type f -name '*.hpp' -exec cp {} ../../include/sun0/{} \;
-find . -type f -name '*.h' -exec cp {} ../../include/sun0/{} \;
-find . -type f -name '*.hpp.in' -exec cp {} ../../include/sun0/{} \;
